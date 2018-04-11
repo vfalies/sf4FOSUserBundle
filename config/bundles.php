cbba2af00ba846781448da65b9816d2e15d45ee1
Ragol <ragolxxx@gmail.com>
2018-04-11 11:02:26 +0200

    Add SwiftMailerBundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
